d80889159dbecb2b7af70f3a70ea02721cbfe667
abhinn14 <abhinnprakashyadav@gmail.com>
2025-09-05 22:33:37 +0530

    fixed

--- api/main.js
+++ api/main.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://chatapy.onrender.com",
+    origin: "http://localhost:5173",
     credentials: true,
 }));
 
