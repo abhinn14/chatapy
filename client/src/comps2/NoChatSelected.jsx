c9b4b3f5ceb3de85cde1cfec8d6fed8eea6134ec
abhinn14 <abhinnprakashyadav@gmail.com>
2025-09-06 17:05:44 +0530

    ui changed

--- client/src/comps2/NoChatSelected.jsx
+++ client/src/comps2/NoChatSelected.jsx
@@ -6,7 +6,7 @@ const NoChatSelected = () => {
       <div className="max-w-md text-center space-y-6">        
         <h2 className="text-2xl font-bold">Welcome to CHATAPY!</h2>
         <p className="text-base-content/60">
-          Select or add someone from sidebar to start chatting.<br/>{"(◔◡◔)"}
+          Always use the same browser for chatting.<br/>You can't read previous messages from a different browser.<br/>{"(◔◡◔)"}
         </p>
       </div>
     </div>
